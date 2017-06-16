356e88031c3724d2166404da1e0dbfc987e53580
Jessica Cacau <jessicaccp@users.noreply.github.com>
2017-06-15 22:44:33 -0300

    Create trabalho.c

---
+++ bonus/trabalho/trabalho.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "vetores.h"
+#include "matrizes.h"
+
+int main () {
+
+    /*
+    receber_vetor();
+    receber_vetor();
+    //receber_vetor();
+    exibir_vetores();
+    soma_vetor(inicio_v, inicio_v->prox);
+    multiplica_vetor(inicio_v, 10);
+    produto_escalar(inicio_v, inicio_v->prox);
+    */
+
+    receber_matriz();
+
+}

a83cfa99c14c850a39b2ffbbb12a739a588b428d
Jessica Cacau <jessicaccp@users.noreply.github.com>
2017-06-16 09:20:18 -0300

    Update trabalho.c

--- bonus/trabalho/trabalho.c
+++ bonus/trabalho/trabalho.c
@@ -5,19 +5,19 @@
 
 int main () {
 
-    /*
     receber_vetor();
     receber_vetor();
-    //receber_vetor();
+    receber_vetor();
     exibir_vetores();
     soma_vetor(inicio_v, inicio_v->prox);
     multiplica_vetor(inicio_v, 10);
     produto_escalar(inicio_v, inicio_v->prox);
-    */
+    somageral_vetor();
 
+
+/*    receber_matriz();
     receber_matriz();
     receber_matriz();
-    receber_matriz();
-    exibir_matrizes();
+    exibir_matrizes();*/
 
 }
